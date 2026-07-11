18c802c0d57e7840961e10a7ae399a945d7cd504
Jyothsna-Priya9676 <ardhamalajyothsnapriya@gmail.com>
2026-07-11 18:53:14 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,37 +1,3 @@
-
-// import { NavLink } from "react-router-dom";
-// import "./Navbar.css";
-// const Navbar = ({ dark, setDark }) => {
-//     return (
-
-//         <nav className="navbar">
-
-//             <div className="logo">
-//                 <span>🎬</span>
-//                 <h1>CineVerse</h1>
-//             </div>
-//             <div className="nav_links">
-//                 <NavLink to="/">Home</NavLink>
-//                 <NavLink to="/favorites">Favorites</NavLink>
-//                 <NavLink to="/trending">Trending</NavLink>
-//                 <NavLink to="/about">About</NavLink>
-//                 <button
-//                     className="theme_btn"
-//                     onClick={() => setDark(!dark)}
-//                 >
-//                     {dark ? "☀️" : "🌙"}
-//                 </button>
-//             </div>
-//         </nav>
-
-//     );
-
-// };
-
-// export default Navbar;
-
-
-
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
